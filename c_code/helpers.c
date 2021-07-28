617c6f26c7c79a21178068a9feefc43aa650b93a
Jan Buzek <jbuzek@cs.washington.edu>
2021-07-28 13:26:51 -0700

    Cleaned up helper file.

--- c_code/helpers.c
+++ c_code/helpers.c
@@ -48,6 +48,7 @@ void add_term(int coeff, int pow, mpz_t current, mpz_t eval) {
         mpz_pow_ui(val, eval, pow);
         mpz_mul_si(val, val, coeff);
         mpz_add(current, val, current);
+        mpz_clear(val);
 }
 
 // fourth polynomial
@@ -58,6 +59,7 @@ bool check_fourth_poly(mpz_t m, mpz_t primes[]) {
 	add_term(2, 1, res1, m);
 	//gmp_printf("First val: %Zd \n", res1);
 	if (! is_smooth(res1, primes, NUM_PRIMES)) {
+		mpz_clear(res1);
 		return false;
 	}
 	mpz_init(res2);
@@ -66,8 +68,12 @@ bool check_fourth_poly(mpz_t m, mpz_t primes[]) {
 	add_term(8, 2, res2, m);
 	//gmp_printf("Second val: %Zd \n", res2);
 	if (! is_smooth(res2, primes, NUM_PRIMES)) {
+		mpz_clear(res1);
+		mpz_clear(res2);
                 return false;
         }
+	mpz_clear(res1);
+	mpz_clear(res2);
 	return true;
 }
 
@@ -78,24 +84,12 @@ bool check_second_poly(mpz_t m, mpz_t primes[]) {
     add_term(2, 1, res1, m);
     //gmp_printf("First val: %Zd \n", res1);
     if (! is_smooth(res1, primes, NUM_PRIMES)) {
+	mpz_clear(res1);
         return false;
     }
+    mpz_clear(res1);
     return true;
 }
-
-
-/*
-long power(int base, int exp) {
-    if (exp == 0)
-        return 1;
-    else if (exp % 2)
-        return base * power(base, exp - 1);
-    else {
-        long temp = power(base, exp / 2);
-        return temp * temp;
-    }
-}
-*/
 
 void smooths_in_range(mpz_t primes[], long min, long max, int num_primes, char* res) { // including min, excluding max
 	long nums[max - min];
@@ -119,77 +113,3 @@ void smooths_in_range(mpz_t primes[], long min, long max, int num_primes, char* 
 		}
 	}
 }
-
-/*
-int main(int argc, char **argv) {
-	long start, end, step;
-	printf("Start: \n");
-	gmp_scanf("%ld", &start);
-	gmp_printf("End: \n");
-	gmp_scanf("%ld", &end);
-	gmp_printf("Step: \n");
-        gmp_scanf("%ld", &step);
-	
-
-	// printf("%ld is start", start);
-
-	mpz_t primes[NUM_PRIMES];
-	mpz_t b;
-	mpz_init_set_si(b, BOUND);
-	primes_up_to_b(primes, b);
-
-	// optimize finding twin smooths	
-	
-	FILE *fp;
-
-	fp = fopen("/tmp/res.txt", "w");
-	
-	long curr_start = start;
-	while (curr_start < end) {
-		char res[step+1]; // cover last pair to overlap
-		for (long i = 0; i < step + 1; i++) {
-			res[i] = 0;
-		}
-
-		smooths_in_range(primes, curr_start, curr_start + step + 1, NUM_PRIMES, res);
-		for (long i = 0; i < step; i++) {
-			if ((res[i] == 1) && (res[i+1] == 1)) {
-				// printf("smooth pair: %ld \n", i + curr_start);
-				mpz_t m;
-				mpz_init_set_si(m, i + curr_start);
-				if (check_poly(m, primes)) {
-                	                printf("SMOOTH: %ld \n ", i + curr_start);
-					char str[257];
-					sprintf(str, "%ld \n", i+curr_start);
-					// str[257] = '\n';
-        	                	fputs(str, fp);
-				}
-	
-			}
-		}
-		
-		curr_start += step;
-		printf("Checked up to %ld.\n", curr_start);
-	}
-	
-	fclose(fp);
-	
-
-	for (long i = start; i < end; i++) {
-		mpz_t m;
-		mpz_t m_plus_one;
-		mpz_init_set_si(m, i);
-		mpz_init_set_si(m_plus_one, (i+1));
-		if (is_smooth(m, primes, NUM_PRIMES) && is_smooth(m_plus_one, primes, NUM_PRIMES)) {
-			if (check_poly(m, primes)) {
-				printf("SMOOTH: %ld \n ", i);
-			}
-		}
-		if (i % 100000 == 0) {
-			printf("Checked up to %ld.\n", i);
-		}
-	}
-
-
-}
-*/
--- c_code/helpers.h
+++ c_code/helpers.h
@@ -2,8 +2,28 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Fills the output parameter ptr with mpz_t values of the prime numbers
+// less than or equal to b. Requires that ptr point to an array with the
+// space needed.
 extern void primes_up_to_b(mpz_t* ptr, mpz_t b);
+// Returns true if and only if arg is smooth, i.e. it factors completely
+// in the set of primes given. num_primes must be the length of primes.
 extern bool is_smooth(mpz_t arg, mpz_t primes[], int num_primes);
-//extern void add_term(int coeff, int pow, mpz_t current, mpz_t eval);
-//extern bool check_fourth_poly(mpz_t m, mpz_t primes[]);
-//extern bool check_second_poly(mpz_t m, mpz_t primes[]);
+// Adds the polynomial term coeff * (eval ^ pow) into current.
+extern void add_term(int coeff, int pow, mpz_t current, mpz_t eval);
+// Returns true if and only if the given integer m corresponds to
+// the nth solution of a Pell equation where the 4n th solution
+// also gives a smooth pair.
+extern bool check_fourth_poly(mpz_t m, mpz_t primes[]);
+// Returns true if and only if the given integer m corresponds to
+// the nth solution of a Pell equation where the 2n th solution
+// also gives a smooth pair.
+extern bool check_second_poly(mpz_t m, mpz_t primes[]);
+// Finds smooths in the range [min, max) that factor in the array
+// of primes given (who's length must be num_primes). Returns this
+// through the output parameter res, which has values set to 1 for
+// smooths (and unchanged values otherwise). Res should have length
+// max - min, so that the ith element corresponds to the integer
+// (min + i).
+extern void smooths_in_range(mpz_t primes[], long min, long max,
+		int num_primes, char* res);
